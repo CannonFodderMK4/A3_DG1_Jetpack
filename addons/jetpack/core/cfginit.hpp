4e5b1f80d7918da10e5e46c3dd027bae9eb5a303
CannonFodderMK4 <60621651+CannonFodderMK4@users.noreply.github.com>
2020-09-09 14:29:00 +0100

    Delete cfginit.hpp

--- addons/jetpack/core/cfginit.hpp
+++
@@ -1,7 +0,0 @@
-class Extended_PreInit_EventHandlers {
-	DG1_Jetpack_PreInit = "call compile preprocessFileLineNumbers 'DG1_Jetpack\data\scripts\XEH_preInit.sqf'";
-};
-
-class Extended_PostInit_EventHandlers {
-	DG1_Jetpack_PostInit = "call compile preprocessFileLineNumbers 'DG1_Jetpack\data\scripts\XEH_postInit.sqf'";
-};
